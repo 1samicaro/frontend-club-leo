1421ec618a2a2fbbafb6a0b907ecbf11ad093c1a
Steven Jaimes Galeano <53667158+Ryudark@users.noreply.github.com>
2024-05-23 11:02:37 -0500

    Update environment.js

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,6 +1,6 @@
 const environment = {
     baseApiUrl: 'https://biblioleo-backend-8420e56b8a6b.herokuapp.com',
-    baseBooksUrl: 'https://biblio-movil-61ddd41755ad.herokuapp.com/'
+    baseBooksUrl: 'https://biblio-back-9df3b84847e8.herokuapp.com/'
 }
 
 export default environment;
