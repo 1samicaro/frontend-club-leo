e6fb337ec2e49874ca8b94fe02a6ec858880925d
1samicaro <123525515+1samicaro@users.noreply.github.com>
2025-08-29 21:24:29 -0500

    Update Regulation.jsx

--- src/view/regulation/Regulation.jsx
+++ src/view/regulation/Regulation.jsx
@@ -44,7 +44,7 @@ export default function Regulation() {
                  de Lectura, responsable de la promoción, vinculación y atención de los socios.
                 </p>
                 <p>
-                <b>Artículo 3. - 0bjetivos.</b> LEO busca alcanzar los siguientes objetivos: 
+                <b>Artículo 2. - 0bjetivos.</b> LEO busca alcanzar los siguientes objetivos: 
                 1. Mejorar el nivel educativo y cultural de las personas, proporcionando medios que
                 promuevan la lectura y la escritura en diferentes idiomas. <br />
                 2. Fortalecer las competencias comunicativas de las personas para mejorar su
@@ -67,7 +67,7 @@ export default function Regulation() {
                 <br />
 
                 <p>
-                <b>Artículo 4. - Socios.</b> Pueden ser socios de LEO las personas naturales en cualquier
+                <b>Artículo 3. - Socios.</b> Pueden ser socios de LEO las personas naturales en cualquier
                 país del mundo, interesadas en participar en los programas de promoción de la lectura,
                 la escritura y el multilingüismo. La prioridad de LEO es la comunidad estudiantil, en
                 cualquier área y nivel de formación. Los menores de edad deberán contar con la autorización 
@@ -76,14 +76,14 @@ export default function Regulation() {
                 </p>
                 <br />
                 <p>
-                <b>Artículo 5. - Costo de Membrecía.</b> Para pertenecer al Club Mundial de Lectura LEO
+                <b>Artículo 4. - Costo de Membrecía.</b> Para pertenecer al Club Mundial de Lectura LEO
                 las personas deberán pagar la cuota de asociación que establezcan las respectivas directivas. <br />
                 PARAGRAFO. Para el año 2025, la cuota semestral de asociación a LEO se establece en
                 COP$100.000 o su equivalente de US$25 ó €$25.
                 </p>
 
                 <p>
-                <b>Artículo 6. – Renovación.</b> Una vez culminado el respectivo semestre, el socio deberá
+                <b>Artículo 5. – Renovación.</b> Una vez culminado el respectivo semestre, el socio deberá
                 renovar la membrecía, para lo cual deberá pagar la respectiva cuota de
                 asociación. El no pago de esta cuota conllevará la pérdida de calidad de socio LEO y
                 sus beneficios. Al vincularse por primera vez, el socio autoriza que LEO pueda
@@ -92,7 +92,7 @@ export default function Regulation() {
                 </p>
                 <br />
                 <p>
-                <b>Artículo 7. - Ingreso.</b> La vinculación se realiza por internet a través del portal
+                <b>Artículo 6. - Ingreso.</b> La vinculación se realiza por internet a través del portal
                 institucional del programa www.clubleo.net. Los interesados deberán diligenciar el
                 formulario de registro proporcionando su información en forma veraz y aportando los
                 soportes requeridos en cada caso. Cualquier falsedad de la información o los soportes
@@ -107,7 +107,7 @@ export default function Regulation() {
                 <h5 align="left"> <b>PROGRAMAS Y SERVICIOS</b></h5>
                 <br />
                 <p>
-                <b>Artículo 8.</b> – El Club Mundial de Lectura LEO ofrece a sus socios programas y servicios
+                <b>Artículo 7.</b> – El Club Mundial de Lectura LEO ofrece a sus socios programas y servicios
                 orientados a promover y masificar la lectura, la escritura y el multilinguismo, para lo
                 cual ha desarrollado y cuenta con los respectivos registros de derechos de autor de los
                 instrumentos tecnológicos web. El acceso a los programas y servicios es gratuito para
@@ -115,47 +115,29 @@ export default function Regulation() {
                 </p>
                 <br />
                 <p>
-                <b>Artículo 9. – Promoción de la Lectura.</b> Los socios LEO pueden acceder gratuitamente
+                <b>Artículo 8. – Promoción de la Lectura.</b> Los socios LEO pueden acceder gratuitamente
                 a los siguientes servicios: <br />
-                Bibliomobil: biblioteca digital con más de 15.000 obras en 5 idiomas (español, inglés,
+                Biblioteca Digital LEO con más de 15.000 obras en 5 idiomas (español, inglés,
                 francés, portugués e italiano) que se adiciona mensualmente con 50 nuevas obras, que
-                los socios pueden acceder y usar a través del portal institucional o descargar a su
+                los socios pueden acceder y usar a través del portal institucional clubleo.net, y leer online o descargar a su
                 dispositivo (PC, Celular o tableta). <br />
-                Bibliotablet: biblioteca digital que los usuarios pueden descargar, de manera que todo
-                el catálogo con las 15.000 obras digitales quedan grabadas en su dispositivo (teléfono
-                celular o tableta), y se pueden consultar y trabajar sin necesidad de conexión a internet. <br />
-                Vamosaleer.com: periódico digital con noticias y artículos sobre géneros, obras y
-                autores, para información y motivación de la lectura y la escritura de los socios LEO. <br />
+                <br />
+                Vamosaleer: periódico digital con noticias y artículos sobre los géneros, obras y
+                autores más represenativos o importantes, para información y motivación de la lectura y la escritura 
+                de los socios LEO, disponible en la página institucional www.vamosaleer.co. <br />
 
-                <b>Artículo 10. – Promoción de la Escritura.</b> LEO ha desarrollado el portal www.escritores.
+                <b>Artículo 9. – Promoción de la Escritura.</b> LEO ha desarrollado el portal www.escritores.
                 leo.com, una red social en la que, gratuitamente, los socios pueden ingresar y crear su
                 perfil de escritor, y subir las obras de su autoría, que podrán ser consultadas por los
                 demás miembros de la comunidad en el país y el mundo. Al Crear su perfil de escritor y
                 subir sus obras, el respectivo socio expresamente garantiza que cumple con las normas
                 sobre derechos de autor vigentes, y autoriza su publicación en el portal para ser
                 consultadas por los demás usuarios.
-                <br />
-                <b>Artículo 11. – Otros servicios.</b> LEO está organizada como una red social mundial de
-                instituciones y personas, que ofrece múltiples oportunidades de contactos, trabajo y
-                negocios para los socios. A mediano plazo, se están desarrollando programas y
-                servicios adicionales, a los que podrán acceder los socios en las siguientes áreas: <br />
-                Taller Literario: Plataforma web educativa para motivar y orientar a los socios en sus
-                actividades de lectura y escritura. <br />
-                Librería. Plataforma de ecomerce para que los socios LEO que tengan obras de su
-                autoría puedan publicarlas, promoverlas y comercializarlas a la comunidad nacional y
-                mundial. La responsabilidad completa de la comercialización, cobro y entrega de las
-                obras corresponde al respectivo socio. LEO sirve de escenario facilitador, pero no tiene
-                responsabilidad alguna en la gestión comercial. <br />
-                Parágrafo. LEO informará a través del portal web institucional sobre la puesta en
-                operación de estos servicios y la forma de acceder a ellos.
-                </p>
-                <br />
-
                 <h4 align="center">CAPITULO IV<br /></h4>
                 <h5 align="left"> <b>MANEJO DE LA INFORMACION</b></h5>
                 <br />
                 <p>
-                <b>Artículo 12. – Seguridad y Confidencialidad.</b> Al registrarse, las personas autorizan a
+                <b>Artículo 10. – Seguridad y Confidencialidad.</b> Al registrarse, las personas autorizan a
                 LEO a utilizar la información suministrada para efectos de identificación, verificación de
                 la calidad de socio, y acceso a los programas y servicios establecidos en el Club
                 Mundial de Lectura. <br />
@@ -168,16 +150,16 @@ export default function Regulation() {
                 </p>
                 <br />
                 <p>
-                <b>Artículo 13. Aprobación.</b> - Acuerdo 01 aprobado por la Asamblea General de
-                Accionistas de la Editorial Educativa Bibliocol SAS, el 19 de febrero de 2024.
+                <b>Artículo 11. Aprobación.</b> - Reglamento 01 aprobado por la Editorial Educativa Bibliocol SAS
+                  y la Asociación LEO Club Mundial de Lectura, en su reunión conjunta del 4 de agosto de 2025.
                 </p>
                 <br />
                 <p>
                 <b>DIANA MARCELA ROMERO ROJAS</b> <br />
-                Gerente General
+                Directora
                 </p>
                 <br />
-                <h4 align="center">Editorial Educativa BIBLIOCOL SAS<br/></h4>
+                
 
             </div>
             </div>
