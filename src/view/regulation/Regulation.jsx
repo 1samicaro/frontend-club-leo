09fd1fb19be4ead9535e8964ad29a9744fdf3065
ryudark <steinu037@gmail.com>
2025-09-10 08:14:56 -0500

    funciona?

--- src/view/regulation/Regulation.jsx
+++ src/view/regulation/Regulation.jsx
@@ -134,6 +134,7 @@ export default function Regulation() {
                 sobre derechos de autor vigentes, y autoriza su publicación en el portal para ser
                 consultadas por los demás usuarios.
                 </p>
+                
                 <h4 align="center">CAPITULO IV<br /></h4>
                 <h5 align="left"> <b>MANEJO DE LA INFORMACION</b></h5>
                 <br />
