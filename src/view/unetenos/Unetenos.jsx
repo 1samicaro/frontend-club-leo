c97cae1cc099fb9d2c483a49f01658e7b08adf57
1samicaro <123525515+1samicaro@users.noreply.github.com>
2025-09-30 19:54:07 -0500

    Update Unetenos.jsx

--- src/view/unetenos/Unetenos.jsx
+++ src/view/unetenos/Unetenos.jsx
@@ -198,7 +198,7 @@ export default function Unetenos() {
 
                 <br />
                 <p>
-                Aunque todas las personas pueden asociarse, la prioridad de LEO son los 476 millones de estudiantes y sus familias en los diferentes países, para quienes la lectura y escritura son actividades permanentes asociadas al proceso de formación académica, y el fortalecimiento de sus competencias comunicativas es clave para el éxito del proceso educativo. 
+                Aunque todas las personas pueden asociarse, la prioridad de <b>LEO</b> son los 476 millones de estudiantes y sus familias en los diferentes países, para quienes la lectura y escritura son actividades permanentes asociadas al proceso de formación académica, y el fortalecimiento de sus competencias comunicativas es clave para el éxito del proceso educativo. 
                     Con la membrecía semestral de sólo COP$100.000, cada socio accede a la biblioteca digital descargable con 15.000 obras en 5 idiomas, valorada en más de COP$150 millones. Es decir, la membrecía permite acceder a obras con un costo promedio de $6 cada una. 
                     Por una monedita de COP$100, recibirás 15 de los mejores libros de literatura y ciencias sociales del mundo!!
                 </p>
