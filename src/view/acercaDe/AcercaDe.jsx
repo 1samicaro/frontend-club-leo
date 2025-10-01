e434c4efe5656e011195abc301334a1036ceb3cb
1samicaro <123525515+1samicaro@users.noreply.github.com>
2025-10-01 10:49:39 -0500

    Update AcercaDe.jsx

--- src/view/acercaDe/AcercaDe.jsx
+++ src/view/acercaDe/AcercaDe.jsx
@@ -176,7 +176,7 @@ export default function AcercaDe() {
                 className="d-block w-100"
                 src={aboutus3}
                 width="400"
-                height="400"
+                height="350"
                 alt="..."
                 />
             </div> 
